67a9e35f9c810fbe713f907778f434a2b1e5c87e
Dhruvi Patel <dhruvi2611patel@gmail.com>
2026-01-06 16:03:15 +0530

    new feature added

---
+++ first.py
@@ -0,0 +1,23 @@
+import streamlit as slt
+import pandas as pd
+from PIL.ImagePalette import random
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df=pd.read_csv("data/Iris.csv")
+slt.title("Iris Flower Classification")
+slt.write(df.head())
+
+x= df.drop(columns=df["Species"])
+y= df["Species"]
+
+x_train , x_test , y_train , y_test = train_test_split(test_size=0.2,randome_state=42)
+model= LogisticRegression()
+model.fit(x_train,y_train)
+
+y_pred= model.predict(y_test)
+slt.write(y_pred)
+
+acc=accuracy_score(x_test,y_pred)
+slt.write("Accuracy-score",acc)
